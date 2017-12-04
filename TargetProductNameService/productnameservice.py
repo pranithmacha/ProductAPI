97ff7a8108664dd7a84da87c3baf0c43904c6815
macha <macha@machas-MBP-2.eau.wi.charter.com>
2017-12-04 08:47:16 -0600

    adding api function docstrings

--- TargetProductNameService/productnameservice.py
+++ TargetProductNameService/productnameservice.py
@@ -1,17 +1,26 @@
 from flask import Flask, request, Response
 import json
+
+""""
+Product name webservice to get and post product name
+endpoints:
+    GET   /products/<id> gives product json with id and name if product exists in dictionary
+    POST  /products  adds a product id and its corresponding name to the dictionary
+"""
 app = Flask(__name__)
-
 products = dict()
-
-
-@app.route("/")
-def hello():
-    return "Hello World!"
 
 
 @app.route("/products/<id>")
 def get_product(id):
+    """
+    api method to get a product details by its id from products dictionary
+    :param id: product id
+    :type id: int
+    :return: json
+    :returns : product id and name if product exists / 404 not found if id is empty /
+               id and None if product is not found
+    """
     _product = dict()
     _product["id"] = id
     _product["name"] = products.get(id, None)
@@ -20,6 +29,13 @@ def get_product(id):
 
 @app.route("/products", methods=['POST'])
 def add_product():
+    """
+    api method to add a product and its name to products dictionary
+    :return: Response object
+    :returns Response 200 on success / Response 400 for missing or invalid data
+    """
+    if not request.data:
+        return Response(status=400)
     id = None
     name = ""
     data = json.loads(request.data)
@@ -28,8 +44,9 @@ def add_product():
             id = val
         if key == "name":
             name = val
+    if not name or not id:
+        return Response(status=400)
     products[id] = name
-    # log.info("adding product {0} {1}".format(id, name))
     print("adding product {0} {1}".format(id, name))
     return Response(status=201)
 
